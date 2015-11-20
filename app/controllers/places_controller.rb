b8da2c48792d51d8cfe30c94073cbad79980ae39
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-11-19 21:37:00 -0300

    added current user to places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_place, only: [:show, :edit, :update, :destroy]
 
   # GET /places
@@ -24,7 +25,8 @@ class PlacesController < ApplicationController
   # POST /places
   # POST /places.json
   def create
-    @place = Place.new(place_params)
+
+    @place = current_user.places.new(place_params)
 
     respond_to do |format|
       if @place.save
@@ -69,6 +71,6 @@ class PlacesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def place_params
-      params.require(:place).permit(:name, :address, :description, :phone, :website, :user_id)
+      params.require(:place).permit(:name, :address, :description, :phone, :website)
     end
 end
